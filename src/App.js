6d9277ebca3b7ec4ad2bf16259c25ac26e04bf76
nthanhvinh160 <nthanhvinh160@gmail.com>
2025-02-04 16:23:51 +0700

    V update 3rd

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+
+import FirstSection from './Components/FirstSection/FirstSection';
 
 function App() {
   return (
     <div className="App">
-
+      <FirstSection />
     </div>
   );
 }
--- src/Components/FirstSection/FirstSection.jsx
+++ src/Components/FirstSection/FirstSection.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
-
+import './FristSection.css'
+import { IconArrowRight } from '../Icon'
 const FirstSection = () => {
   return (
     <>
       <div>
         <div className='header_menu'>
           <div className='button_right_arrow'>
-
+            <IconArrowRight />
+          </div>
+          <div className='menu_wrapper'>
+            <ul className="list_menu">
+              <li className="list_menu_item">Tutorials</li>
+              <li className="list_menu_item">Case studies</li>
+              <li className="list_menu_item">Resources</li>
+            </ul>
           </div>
         </div>
       </div>
